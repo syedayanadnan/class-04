5080849b9203e7933ab33fcf0283a9af002aad55
syedayanadnan <youremail@example.com>
2024-02-20 13:08:08 +0500

    first commit

---
+++ 01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 110101
+let accountEmail = "ayanadnan9955@gmail.com"
+var accountPassword = "123456789"
+accountCity = "jaipur"
+let accountState;
+ //accountId = 2
+accountEmail = "adnaankhan@gmail.com"
+ console.table([accountEmail,accountPassword,accountCity,accountState])
+ accountPassword = "12345" 
+ accountCity = "karachi"
+ /*
+ prefer not to use var
+ because of issue in block scope and functional scope
+ */
